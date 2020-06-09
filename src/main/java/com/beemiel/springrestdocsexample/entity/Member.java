c79e8d75bb2a9069790012b17ff8e5c43e04cdc2
beemiel <qnswwrn@gmail.com>
2020-06-09 20:20:51 +0900

    feat: member 생성자 추가

--- src/main/java/com/beemiel/springrestdocsexample/entity/Member.java
+++ src/main/java/com/beemiel/springrestdocsexample/entity/Member.java
@@ -12,4 +12,13 @@ public class Member {
     private String name;
     private int age;
 
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member(Long id, int age) {
+        this.id = id;
+        this.age = age;
+    }
 }
