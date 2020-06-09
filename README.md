bc60965ea2294fa1d7df2a4795e25b8fe584bc19
beemiel <qnswwrn@gmail.com>
2020-06-09 15:24:39 +0900

    feat: 새로운 프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,1 @@
+### Spring REST Docs 예제
---
+++ src/test/java/com/beemiel/springrestdocsexample/SpringRestDocsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beemiel.springrestdocsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
